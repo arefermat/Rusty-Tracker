8153d6ef600b90868a451a6e88504632ba579a0a
Alex Refermat <73394181+arefermat@users.noreply.github.com>
2025-03-31 21:00:55 -0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,4 +1,67 @@
-fn main() {
-    println!("Hello world!")
+use clap::{Parser, Subcommand};
+
+
+#[derive(Parser)]
+struct Args {
+    #[command(subcommand)]
+    cmd : Commands,
 }
 
+#[derive(Subcommand)]
+#[derive(Debug)]
+enum IsDone {
+    Done,
+    Incomplete,
+}
+#[derive(Subcommand)]
+enum Commands {
+    New {
+        assigment_name : String,
+        due_date : String,
+        time_estimate : i32
+    },
+    View {assigment_name : String},
+    Mark {
+        assigment_name : String,
+        #[command(subcommand)]
+        done : IsDone
+    },
+    Resources {
+        subject : String,
+    }
+}
+
+//Command Functions Below:
+
+fn new_assigment(name: String, due_date: String, time_estimate: i32) {
+    println!("Creating a new assigment with name {}, due date of {} and that'll take about {} minute to complete.", name, due_date, time_estimate);
+    
+}
+
+fn view_assigment(name: String) {
+    println!("Viewing an assigment with name {}", name);
+}
+
+fn mark_assigment(name: String, done: IsDone) {
+    println!("Marking assigment named {} as {:?}", name, done);
+}
+
+fn resources(subject: String) {
+    println!("Here are a list resources for the subject of {}: ", subject)
+}
+
+fn main() {
+
+    let args: Args = Args::parse();
+    
+    match args.cmd {
+        Commands::New { assigment_name, due_date, time_estimate } => new_assigment(assigment_name, due_date, time_estimate),
+        Commands::View { assigment_name } => view_assigment(assigment_name),
+        Commands::Mark { assigment_name, done } => match done {
+            IsDone::Done => mark_assigment(assigment_name, IsDone::Done),
+            IsDone::Incomplete => mark_assigment(assigment_name, IsDone::Incomplete),
+        },
+        Commands::Resources { subject } => resources(subject),
+    }
+
+}
